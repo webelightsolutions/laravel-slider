6f5642ac53ed121b83c755a0c1fdacff66a0d3d3
jitendrabavliya <pateljitendra3fw@gmail.com>
2017-11-17 12:08:31 +0530

    remove unwanted folder and file

--- routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::get('slider/changeStatus/{id}', 'Webelightdev\LaravelSlider\Controller\SliderController@formActions');
-Route::post('slides/preview', 'Webelightdev\LaravelSlider\Controller\SliderController@preview');
-
-Route::resource('slider', 'Webelightdev\LaravelSlider\Controller\SliderController');
